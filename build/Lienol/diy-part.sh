376e39daad7ab9151d666920f9daa72c597b23b0
m215owen <39959893+m215owen@users.noreply.github.com>
2023-04-16 15:03:36 +0800

    Update diy-part.sh

--- build/Lienol/diy-part.sh
+++ build/Lienol/diy-part.sh
@@ -4,6 +4,10 @@
 # 自行拉取插件之前请SSH连接进入固件配置里面确认过没有你要的插件再单独拉取你需要的插件
 # 不要一下就拉取别人一个插件包N多插件的，多了没用，增加编译错误，自己需要的才好
 
+echo '添加lwz322的K3屏幕插件'
+rm -rf package/luci-app-k3screenctrl
+git clone https://github.com/lwz322/luci-app-k3screenctrl.git package/luci-app-k3screenctrl
+echo '=========Add k3screen plug OK!========='
 
 # 后台IP设置
 export Ipv4_ipaddr="192.168.3.1"            # 修改openwrt后台地址(填0为关闭)
